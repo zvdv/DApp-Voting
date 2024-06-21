aa2dda98814796a6c6188c49268aeae9b344dabd
zvdv <156561301+zvdv@users.noreply.github.com>
2024-06-21 11:34:59 -0700

    Vote function

--- vote.sol
+++ vote.sol
@@ -10,4 +10,17 @@ contract Vote {
     constructor(string[] memory _candidates) {
         candidates = _candidates;
     }
+
+    function vote (string memory _candidate) validCandidate(_candidate) public {
+        votes[_candidate]++;
+    }
+
+    modifier validCandidate(string memory _candidate) {
+        for (uint256 i = 0; i < candidates.length; i++) {
+            if (keccak256(abi.encodePacked(candidates[i])) == keccak256(abi.encodePacked(_candidate))) {
+                _;
+            }
+        }
+        revert("Invalid candidate.");
+    }
 }
