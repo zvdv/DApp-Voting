43d75e366cf5769d33b4daf3c22b5724efa5baef
zvdv <156561301+zvdv@users.noreply.github.com>
2024-07-09 20:42:20 -0700

    Possibly givenProvider will be useful at some point

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import Web3 from 'web3';
 import VoteMetaData from '../artifacts/Vote_metadata.json';
 
 const VoteContractABI = VoteMetaData.abi;
-const web3 = new Web3(new Web3.providers.HttpProvider('https://rpc.sepolia.org'));
+const web3 = new Web3(Web3.givenProvider || new Web3.providers.HttpProvider('https://rpc.sepolia.org'));
 
 // if (MetaMaskProvider.window.ethereum){
 // } else {
@@ -13,7 +13,7 @@ const web3 = new Web3(new Web3.providers.HttpProvider('https://rpc.sepolia.org')
 //     const web3 = new Web3();
 // }
 const contractAddress = '0x8A0F33F92ba8C6fCA1a35dD9a5b0f571920a1ca6';
-//const contract = new web3.eth.Contract(VoteContractABI, contractAddress);
+const contract = new web3.eth.Contract(VoteContractABI, contractAddress);
 
 //const candidates = contract.methods.getCandidates().call();
 
